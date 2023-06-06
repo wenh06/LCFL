6132fa8e22144dde1034befde4483a997895aa5b
WEN Hao <wenh06@gmail.com>
2023-06-06 16:45:23 +0800

    update cli.py

--- cli.py
+++ cli.py
@@ -5,14 +5,10 @@ Reads in a yaml file with experiment parameters and runs the experiment.
 """
 
 import argparse
-import os
-import re
 import sys
-from collections import OrderedDict
 from copy import deepcopy
-from itertools import product
 from pathlib import Path
-from typing import List, Union
+from typing import List
 
 sys.path.append(str(Path(__file__).parent / "fl-sim"))
 
@@ -25,12 +21,17 @@ from fl_sim.data_processing import (
     FedProxMNIST,
 )
 from fl_sim.algorithms import fedopt, fedprox
+from fl_sim.cli import parse_config_file
 from torch_ecg.cfg import CFG
-import yaml
 
-from dataset import FedRotatedMNIST, FedRotatedCIFAR10
 from algorithm import LCFLServer, LCFLServerConfig, LCFLClientConfig
+from algorithm_prox import (
+    LCFLServer as LCFLProxServer,
+    LCFLServerConfig as LCFLProxServerConfig,
+    LCFLClientConfig as LCFLProxClientConfig,
+)
 from ifca import IFCAServer, IFCAServerConfig, IFCAClientConfig
+from dataset import FedRotatedMNIST, FedRotatedCIFAR10
 
 
 # create log directory if it does not exist
@@ -60,62 +61,6 @@ def parse_args() -> List[CFG]:
     return parse_config_file(config_file_path)
 
 
-def parse_config_file(config_file_path: Union[str, Path]) -> List[CFG]:
-    file_content = Path(config_file_path).read_text()
-
-    configs = yaml.safe_load(file_content)
-
-    # set env if specified
-    if configs.get("env", None) is not None:
-        for k, v in configs["env"].items():
-            os.environ[k] = str(v)
-
-    # further process configs to a list of configs
-    # by replacing values of the pattern ${{ matrix.key }} with the value of key
-    # specified by configs["strategy"]["matrix"][key]
-    strategy_matrix = OrderedDict(configs["strategy"]["matrix"])
-    repalcements = list(product(*strategy_matrix.values()))
-    keys = list(strategy_matrix.keys())
-    configs = []
-    for rep in repalcements:
-        new_file_content = deepcopy(file_content)
-        for k, v in zip(keys, rep):
-            # replace all patterns of the form ${{ matrix.k }} in file_content with v
-            # pattern = re.compile(f"\${{{{ matrix.{k} }}}}")
-            # allow for arbitrary number (can be 0) of spaces around matrix.k
-            pattern = re.compile(f"\\${{{{(?:\\s+)?matrix.{k}(?:\\s+)?}}}}")
-            new_file_content = re.sub(
-                pattern,
-                re.sub("(\\n[\\.]{3})?\\n$", "", yaml.safe_dump(v)),
-                new_file_content,
-            )
-        new_config = CFG(yaml.safe_load(new_file_content))
-        new_config.pop("strategy")
-        # replace pattern of the form ${{ xx.xx... }} with corresponding value
-        pattern = re.compile(
-            "\\$\\{\\{ (?:\\s+)?(?P<repkey>\\w[\\.\\w]+\\w)(?:\\s+)?\\}\\}"
-        )
-        matches = re.finditer(pattern, new_file_content)
-        for match in matches:
-            repkey = match.group("repkey")
-            try:
-                repval = eval(f"new_config.{repkey}")
-            except Exception:
-                raise ValueError(f"Invalid key {repkey} in {config_file_path}")
-            rep_pattern = re.compile(f"\\$\\{{{{(?:\\s+)?{repkey}(?:\\s+)?}}}}")
-            new_file_content = re.sub(
-                rep_pattern,
-                re.sub("(\\n[\\.]{3})?\\n$", "", yaml.safe_dump(repval)),
-                new_file_content,
-                count=1,
-            )
-        new_config = CFG(yaml.safe_load(new_file_content))
-        new_config.pop("strategy")
-        configs.append(new_config)
-
-    return configs
-
-
 def single_run(config: CFG):
     # run a single experiment
     config = CFG(config)
@@ -127,6 +72,11 @@ def single_run(config: CFG):
                 "server_config": LCFLServerConfig,
                 "client_config": LCFLClientConfig,
                 "server": LCFLServer,
+            },
+            "LCFL_Prox": {
+                "server_config": LCFLProxServerConfig,
+                "client_config": LCFLProxClientConfig,
+                "server": LCFLProxServer,
             },
             "IFCA": {
                 "server_config": IFCAServerConfig,
